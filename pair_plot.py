d02798b84aa85d8cf18547704dbfd5cb6ee2daf2
Justine Munoz <jumunoz@student.42nice.fr>
2025-01-06 11:48:09 +0100

    hehe

--- pair_plot.py
+++ pair_plot.py
@@ -6,26 +6,31 @@ import matplotlib.pyplot as plt
 
 
 def pair_plot(data):
-    features = [
-        "Arithmancy",
-        "Astronomy",
-        "Herbology",
-        "Defense Against the Dark Arts",
-        "Divination",
-        "Muggle Studies",
-        "Ancient Runes",
-        "History of Magic",
-        "Transfiguration",
-        "Potions",
-        "Care of Magical Creatures",
-        "Charms",
-        "Flying",
-        "Hogwarts House"
-    ]
+    features = {
+        "Arithmancy": "Arith",
+        "Astronomy": "Astro",
+        "Herbology": "Herbo",
+        "Defense Against the Dark Arts": "DADA",
+        "Divination": "Divin",
+        "Muggle Studies": "Muggle",
+        "Ancient Runes": "Runes",
+        "History of Magic": "History",
+        "Transfiguration": "Transf",
+        "Potions": "Potions",
+        "Care of Magical Creatures": "CMC",
+        "Charms": "Charms",
+        "Flying": "Flying",
+        "Hogwarts House": "Houses"
+    }
 
-    df = pd.DataFrame(data, columns=features)
+    df = pd.DataFrame(data, columns=features.keys())
+    df = df.rename(columns=features)
 
-    sns.pairplot(df, hue="Hogwarts House", kind="scatter", diag_kind="hist")
+    sns.pairplot(df, hue="Houses", kind="scatter", diag_kind="hist", height=1.5)
+
+    plt.gcf().set_size_inches(15, 20)
+
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
     plt.show()
 
